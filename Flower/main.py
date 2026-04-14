52b32f8a0bf20bdb0f2f5ffb8142988c547c6e45
Gitansh Agalcha <gitanshagalcha@gmail.com>
2026-04-14 10:49:29 +0530

    Create main.py

---
+++ Flower/main.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+t.bgcolor("black")
+t.color("lavender")
+
+for i in range(1, 120):
+    t.left(200)
+    t.forward(100)
+    t.circle(-90)
+    t.circle(-100)
+    t.speed(20)
+
+t.done()
